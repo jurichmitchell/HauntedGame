0df044be8b204f12e5d0d15e35f99d3e0e6242cd
Mitchell Jurich <mdj50790@ucmo.edu>
2019-12-25 00:57:35 -0600

    Camera now follows player

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+	public GameObject focusObj;
+
+	// Start is called before the first frame update
+	void Start() {
+
+	}
+
+	// Update is called once per frame
+	void LateUpdate() {
+		updatePosition();
+	}
+
+	void updatePosition() {
+		float xPos = focusObj.transform.position.x;
+		float yPos = focusObj.transform.position.y;
+		this.transform.position = new Vector3(xPos, yPos, -10.0f);
+	}
+}
