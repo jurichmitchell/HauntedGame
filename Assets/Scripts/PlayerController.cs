7afd51c43a424898c9f9caed7a31f72b01c2a78c
Mitchell Jurich <mdj50790@ucmo.edu>
2019-12-29 18:23:07 -0600

    Modified player movement
    Player now uses deltaTime and has running capabilities

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,12 +4,14 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 	[Tooltip("The speed multiplier for player movement.")]
-		[SerializeField] private float speed = 0.05f;
+		[SerializeField] private float speed = 5f;
+	public float runSpeedMod = 5f;
 
+	private bool running;
 
 	// Start is called before the first frame update
 	void Start() {
-		
+		running = false;
 	}
 
 	// Update is called once per frame
@@ -18,8 +20,16 @@ public class PlayerController : MonoBehaviour {
 	}
 
 	void handleMovement() {
-		float xPos = this.transform.position.x + Input.GetAxis("Horizontal") * speed;
-		float yPos = this.transform.position.y + Input.GetAxis("Vertical") * speed;
+		// Check if running
+		running = Input.GetButton("Run");
+
+		// Determine the player's current speed
+		float currSpeed = speed;
+		currSpeed += running ? runSpeedMod : 0;
+
+		// Get new position
+		float xPos = this.transform.position.x + Input.GetAxis("Horizontal") * currSpeed * Time.deltaTime;
+		float yPos = this.transform.position.y + Input.GetAxis("Vertical") * currSpeed * Time.deltaTime;
 		this.transform.position = new Vector3(xPos, yPos, 0.0f);
 	}
 }
